5fed4fcaff40c91d6480aa99a38b336c953959da
stajun <stajun@naver.com>
2020-07-09 13:06:10 +0900

    게시판 리스트 구현 완료

---
+++ spring/src/main/java/kr/green/spring/dao/BoardDao.java
@@ -0,0 +1,15 @@
+package kr.green.spring.dao;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Param;
+
+import kr.green.spring.vo.BoardVo;
+
+public interface BoardDao {
+
+	ArrayList<BoardVo> getBoardList();
+
+	BoardVo getBoard(@Param("num")Integer num);
+
+}
---
+++ spring/src/main/java/kr/green/spring/service/BoardServiceImp.java
@@ -0,0 +1,25 @@
+package kr.green.spring.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.green.spring.dao.BoardDao;
+import kr.green.spring.vo.BoardVo;
+
+@Service
+public class BoardServiceImp implements BoardService {
+	@Autowired
+	private BoardDao boardDao;
+
+	@Override
+	public ArrayList<BoardVo> getBoardList() {
+		return boardDao.getBoardList();
+	}
+
+	@Override
+	public BoardVo getBoard(Integer num) {
+		return boardDao.getBoard(num);
+	}
+}
