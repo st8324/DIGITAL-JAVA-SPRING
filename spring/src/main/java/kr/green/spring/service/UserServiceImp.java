2999c0c4c3be75a5ea8d541294b119782c504691
stajun <stajun@naver.com>
2020-07-09 09:35:24 +0900

    타일즈 설정 및 DB 연동 완료

---
+++ spring/src/main/java/kr/green/spring/service/UserServiceImp.java
@@ -0,0 +1,24 @@
+package kr.green.spring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.green.spring.dao.UserDao;
+
+@Service
+public class UserServiceImp implements UserService {
+
+	@Autowired
+	private UserDao userDao;
+	
+	@Override
+	public String getPw(String id) {
+		return userDao.getPw(id);
+	}
+
+	@Override
+	public int getCount() {
+		return userDao.getCount();
+	}
+	
+}
